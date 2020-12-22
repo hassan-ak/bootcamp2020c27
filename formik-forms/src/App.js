b0c6804854c6d6cdc0d2da9179312af98b78c1e7
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-12-22 06:57:38 +0500

    Step03
    1:- installed YUP using "npm install yup --save"
    2:- create form with Yup validation

--- formik-forms/src/App.js
+++ formik-forms/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import UserForm from './UserForm';
 import { UserFormWithFormik } from './UserFormWithFormik';
+import { UserFormWithFormikValidation } from './UserFormWithFormikValidation';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <UserForm/>
       <hr/>
       <UserFormWithFormik/>
+      <hr/>
+      <UserFormWithFormikValidation/>
     </div>
   );
 }
